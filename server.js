869a23a9eea840583afe20047313a7146e9dd91e
Ananda Wiradharma <anandawira754@gmail.com>
2021-07-19 14:42:57 +0700

    done

--- server.js
+++ server.js
@@ -21,13 +21,24 @@ mongoose.connect(
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.render('index');
+app.get('/', async (req, res) => {
+	const shortUrls = await ShortUrl.find();
+	res.render('index', { shortUrls });
 });
 
 app.post('/shortUrls', async (req, res) => {
 	await ShortUrl.create({ full: req.body.fullUrl });
 	res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+	const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+	if (shortUrl == null) return res.sendStatus(404);
+
+	shortUrl.clicks++;
+	shortUrl.save();
+
+	res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 3000);
